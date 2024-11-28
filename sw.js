f6e82b1747f61ca65b0262289a5272191bf7c00a
kumoko <453513858@qq.com>
2024-11-28 20:18:21 +0800

    Site updated: 2024-11-28 20:18:20

--- sw.js
+++ sw.js
@@ -6,7 +6,7 @@ const preCache = [
   "/css/style.css",
   "/js/script.js",
 ];
-const VERSION = "1732795298568";
+const VERSION = "1732796293530";
 const cacheDomain = [
   "fonts.googleapis.com",
   "npm.webcache.cn",
